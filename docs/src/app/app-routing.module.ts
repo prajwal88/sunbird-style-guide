acef2c18600ef593a2dff59b405c5c0445314986
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-03-06 16:37:32 +0530

    Issue #000 fix: update

---
+++ docs/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TypographyComponent } from './typography/typography.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { LabelsComponent } from './labels/labels.component';
+import { ModalsComponent } from './modals/modals.component';
+
+
+const routes: Routes = [
+  { path: '', component: TypographyComponent},
+  { path: 'typography', component: TypographyComponent },
+  { path: 'buttons', component: ButtonsComponent },
+  { path: 'labels', component: LabelsComponent },
+  { path: 'modals', component: ModalsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ docs/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { ModalsComponent } from './modals/modals.component';
+import { LabelsComponent } from './labels/labels.component';
+import { TypographyComponent } from './typography/typography.component';
+import { OverviewComponent } from './overview/overview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    ButtonsComponent,
+    ModalsComponent,
+    LabelsComponent,
+    TypographyComponent,
+    OverviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
