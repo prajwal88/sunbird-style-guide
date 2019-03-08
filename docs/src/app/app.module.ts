95c9e0551dd29b5aa1e9f4598125f0d5f9c89878
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-03-08 12:19:00 +0530

    stable

--- docs/src/app/app.module.ts
+++ docs/src/app/app.module.ts
@@ -3,6 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightModule } from 'ngx-highlightjs';
+import xml from 'highlight.js/lib/languages/xml';
+import scss from 'highlight.js/lib/languages/scss';
+import typescript from 'highlight.js/lib/languages/typescript';
+import html from 'highlight.js/lib/languages/htmlbars';
+import { ClipboardModule } from 'ngx-clipboard';
+
+import { EscapeHtmlPipe } from './pipes/keep-html.pipe';
+/**
+ * Import every language you wish to highlight here
+ * NOTE: The name of each language must match the file name its imported from
+ */
+export function hljsLanguages() {
+  return [
+    {name: 'html', func: html},
+    {name: 'typescript', func: typescript},
+    {name: 'scss', func: scss},
+    {name: 'xml', func: xml}
+  ];
+}
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { ModalsComponent } from './modals/modals.component';
@@ -13,6 +34,7 @@ import { OverviewComponent } from './overview/overview.component';
 @NgModule({
   declarations: [
     AppComponent,
+    EscapeHtmlPipe,
     SidebarComponent,
     ButtonsComponent,
     ModalsComponent,
@@ -22,7 +44,9 @@ import { OverviewComponent } from './overview/overview.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighlightModule.forRoot({ languages: hljsLanguages }),
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- docs/src/app/buttons/buttons.component.ts
+++ docs/src/app/buttons/buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightResult } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-buttons',
@@ -9,7 +10,84 @@ export class ButtonsComponent implements OnInit {
 
   constructor() { }
 
+  pageTitle = 'Buttons';
+  sections = [
+    {
+      expandCode: false,
+      title: 'Basic Buttons',
+      demoCode: `
+      <button class="sb-btn sb-btn-outline-primary sb-btn-normal mr-10">Basic Button</button>
+      <button class="sb-btn sb-btn-outline-secondary sb-btn-normal mr-10">Secondary</button>
+      <button class="sb-btn sb-btn-outline-tertiary sb-btn-normal mr-10">Tertiary</button>
+      <button class="sb-btn sb-btn-outline-info sb-btn-normal mr-10">Info</button>
+      <button class="sb-btn sb-btn-outline-success sb-btn-normal mr-10">Success</button>
+      <button class="sb-btn sb-btn-outline-warning sb-btn-normal mr-10">Warning</button>
+      <button class="sb-btn sb-btn-outline-error sb-btn-normal mr-10">Error</button>
+      `,
+      copyCode: `
+      <button class="sb-btn sb-btn-outline-primary sb-btn-normal">Basic Button</button>
+      <button class="sb-btn sb-btn-outline-secondary sb-btn-normal">Secondary</button>
+      <button class="sb-btn sb-btn-outline-tertiary sb-btn-normal">Tertiary</button>
+      <button class="sb-btn sb-btn-outline-info sb-btn-normal">Info</button>
+      <button class="sb-btn sb-btn-outline-success sb-btn-normal">Success</button>
+      <button class="sb-btn sb-btn-outline-warning sb-btn-normal">Warning</button>
+      <button class="sb-btn sb-btn-outline-error sb-btn-normal">Error</button>
+      `
+    },
+    {
+      expandCode: false,
+      title: 'Button States',
+      demoCode: `
+      <button class="sb-btn sb-btn-disabled sb-btn-normal mr-10">Primary Disable</button>
+      <button class="sb-btn sb-btn-outline-disabled sb-btn-normal">Basic Button Disable</button>
+      `,
+      copyCode: `
+      <button class="sb-btn sb-btn-disabled sb-btn-normal">Primary Disable</button>
+      <button class="sb-btn sb-btn-outline-disabled sb-btn-normal">Basic Button Disable</button>
+      `
+    },
+    {
+      expandCode: false,
+      title: 'Colored Buttons',
+      demoCode: `
+      <button class="sb-btn sb-btn-primary sb-btn-normal mr-10">Primary Button</button>
+      <button class="sb-btn sb-btn-secondary sb-btn-normal mr-10">Secondary</button>
+      <button class="sb-btn sb-btn-tertiary sb-btn-normal mr-10">Tertiary</button>
+      <button class="sb-btn sb-btn-warning sb-btn-normal mr-10">warning</button>
+      <button class="sb-btn sb-btn-error sb-btn-normal mr-10">Error</button>
+      <button class="sb-btn sb-btn-info sb-btn-normal mr-10">Info</button>
+      `,
+      copyCode: `
+      <button class="sb-btn sb-btn-primary sb-btn-normal">Primary Button</button>
+      <button class="sb-btn sb-btn-secondary sb-btn-normal">Secondary</button>
+      <button class="sb-btn sb-btn-tertiary sb-btn-normal">Tertiary</button>
+      <button class="sb-btn sb-btn-warning sb-btn-normal">warning</button>
+      <button class="sb-btn sb-btn-error sb-btn-normal">Error</button>
+      <button class="sb-btn sb-btn-info sb-btn-normal">Info</button>
+      `
+    },
+    {
+      expandCode: false,
+      title: 'Button Sizes',
+      demoCode: `
+      <button class="sb-btn sb-btn-primary sb-btn-xs mr-10">mini</button>
+      <button class="sb-btn sb-btn-primary sb-btn-normal mr-10">normal</button>
+      <button class="sb-btn sb-btn-primary sb-btn-sm mr-10">small</button>
+      <button class="sb-btn sb-btn-primary sb-btn-md mr-10">medium</button>
+      <button class="sb-btn sb-btn-primary sb-btn-lg mr-10">large</button>
+      `,
+      copyCode: `
+      <button class="sb-btn sb-btn-primary sb-btn-xs">mini</button>
+      <button class="sb-btn sb-btn-primary sb-btn-normal">normal</button>
+      <button class="sb-btn sb-btn-primary sb-btn-sm">small</button>
+      <button class="sb-btn sb-btn-primary sb-btn-md">medium</button>
+      <button class="sb-btn sb-btn-primary sb-btn-lg">large</button>
+      `
+    }
+  ];
+
   ngOnInit() {
   }
+
 
 }
